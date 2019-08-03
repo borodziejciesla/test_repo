03c402818fa6adb7a593fc6668c13c335018ab61
Maciej Różewicz <rozewicz.maciej@gmail.com>
2019-08-03 13:40:56 +0200

    added code for i2c

--- HelloSTM32/src/main.c
+++ HelloSTM32/src/main.c
@@ -1,39 +1,48 @@
-#include <string.h>
-
 #include "stm32f1xx.h"
-#include "initGPIO.h"
-#include "globals.h"
 
-/************************************************************************************************/
+I2C_HandleTypeDef i2c;
+
+void delay(int time)
+{
+    int i;
+    for (i = 0; i < time * 570; i++) {}
+}
+
 int main(void)
 {
-	SystemCoreClock = 8000000; // 8Mhz
-
-	float measurement_step_time = 0.005f;
-
-	/* Initialize HAl and clock */
+	SystemCoreClock = 8000000; // taktowanie 8Mhz
 	HAL_Init();
+
 	__HAL_RCC_GPIOA_CLK_ENABLE();
+	__HAL_RCC_GPIOB_CLK_ENABLE();
 	__HAL_RCC_GPIOC_CLK_ENABLE();
-	__HAL_RCC_TIM2_CLK_ENABLE();
-	__HAL_RCC_USART2_CLK_ENABLE();
+	__HAL_RCC_I2C1_CLK_ENABLE();
 
-	/* Initialize peripherals */
-	initLED(getGPIO());
-	initTimerComparator(getGlobalTimer(), measurement_step_time);
-	USARTInit(getGPIO(), getUart());
-	initComparatorInput(getGPIO());
+	GPIO_InitTypeDef gpio;
+	gpio.Mode = GPIO_MODE_AF_OD;
+	gpio.Pin = GPIO_PIN_6 | GPIO_PIN_7; // SCL, SDA
+	gpio.Pull = GPIO_PULLUP;
+	gpio.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOB, &gpio);
 
-	/* Initialize interruptions */
-	HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
-	HAL_NVIC_EnableIRQ(TIM2_IRQn);
-	HAL_NVIC_EnableIRQ(USART2_IRQn);
+	i2c.Instance             = I2C1;
+	i2c.Init.ClockSpeed      = 100000;
+	i2c.Init.DutyCycle       = I2C_DUTYCYCLE_2;
+	i2c.Init.OwnAddress1     = 0xff;
+	i2c.Init.AddressingMode  = I2C_ADDRESSINGMODE_7BIT;
+	i2c.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
+	i2c.Init.OwnAddress2     = 0xff;
+	i2c.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
+	i2c.Init.NoStretchMode   = I2C_NOSTRETCH_DISABLE;
 
-	/* Start modules */
-	HAL_TIM_Base_Start_IT(getGlobalTimer());
-	//__HAL_TIM_ENABLE_IT(getGlobalTimer(), TIM_IT_CC1);
+	HAL_I2C_Init(&i2c);
 
+	uint8_t test = 0x5a;
+	HAL_I2C_Mem_Write(&i2c, 0x48, 0x10, 1, (uint8_t*)&test, 0, HAL_MAX_DELAY);
 
+	uint8_t result = 0;
+	//HAL_I2C_Mem_Read(&i2c, 0x48, 0x10, 1, (uint8_t*)&result, sizeof(result), HAL_MAX_DELAY);
 
-	while (1) {}
+	while (1)
+	{delay(50);}
 }
